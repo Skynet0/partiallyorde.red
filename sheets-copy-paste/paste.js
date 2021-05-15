b2e3e75f4adbb1bec5002a7317ac264423bb6461
Skynet0 <ethan.ordentlich2017@gmail.com>
2021-05-15 06:01:05 -0700

    Improve handling of merged cells

--- sheets-copy-paste/paste.js
+++ sheets-copy-paste/paste.js
@@ -22,7 +22,8 @@
     // Pre-dom batch replacements, that are easier to do as strings
     // rather than iterating and applying.
 
-    // Pasting into sheets can't parse this hex border color for some reason
+    // Pasting into sheets can't parse hex border colors for some reason
+    // TODO: replace this with a hex -> rgb(r,g,b)
     html = html.replaceAll("#000000", "black");
 
     // Fine-grained DOM modifications
@@ -44,9 +45,44 @@
     table.removeAttribute("border");
     table.style.removeProperty("border");
 
-    // Add a dummy row so that the last cell still has its styles
-    table.getElementsByTagName("tbody")[0].appendChild(gen("tr"));
+    let tbody = table.getElementsByTagName("tbody")[0];
 
+    // CHROME
+    // Add a dummy row so that the last cell still has its styles
+    tbody.insertRow(-1);
+
+    // Iterate over
+    let tds = tbody.getElementsByTagName("td");
+    for (let i = 0; i < tds.length; i++) {
+      let td = tds[i];
+
+      // If a cell consists of solely whitespace, then it loses styling
+      if (td.colSpan > 1 && td.textContent.trim().length === 0) {
+        // FIREFOX: set data-sheets-value attribute
+        if (!td.hasAttribute("data-sheets-value")) {
+          td.setAttribute(
+            "data-sheets-value",
+            JSON.stringify({
+              1: 2,
+              2: " ",
+            })
+          );
+        }
+        // CHROME: use a ' to indicate literally empty
+        td.textContent = "'" + td.textContent;
+      }
+    }
+
+    target.appendChild(document.createComment("StartFragment"));
+
+    let meta = gen("meta");
+    meta.name = "generator";
+    meta.content = "Sheets";
+
+    // TODO: try out an iframe for generating the content?
+    // It would help with style independence.
+
+    target.appendChild(meta);
     target.appendChild(sheetsBase);
   }
 
